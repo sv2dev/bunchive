101542d3c8c96a5f035e95390a6f83c5dc93709f
Sven Rogge <mail@sven-rogge.com>
2026-01-04 22:02:36 +0100

    Add padding to iso timestamp

--- src/cli.ts
+++ src/cli.ts
@@ -22,9 +22,9 @@ type Command = (typeof commands)[number];
 
 const usage = `\
 Usage:
-  br backup [options] <...sources>
-  br restore [options] <source>
-  br key
+  bunchive backup [options] <...sources>
+  bunchive restore [options] <source>
+  bunchive key
 
 If used with s3, you need to provide the credentials via environment variables.
 
@@ -167,9 +167,12 @@ function createTimestamp(format: TimestampFormat): string {
   if (format === "none") return "";
   if (format === "unix") return Math.floor(Date.now() / 1000).toString();
   const d = new Date();
-  return `${d.getFullYear()}-${
-    d.getMonth() + 1
-  }-${d.getDate()}T${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}`;
+  return `${d.getFullYear()}-${(d.getMonth() + 1)
+    .toString()
+    .padStart(
+      2,
+      "0",
+    )}-${d.getDate().toString().padStart(2, "0")}T${d.getHours().toString().padStart(2, "0")}-${d.getMinutes().toString().padStart(2, "0")}-${d.getSeconds().toString().padStart(2, "0")}`;
 }
 
 function createBackupFilename(
